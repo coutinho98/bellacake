04aa9bb535fb2f026c1de9799839d51c829909a7
coutinho98 <mateuscouto98@outlook.com>
2025-03-07 07:04:26 -0300

    test of colors and responsivity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
     return (
         <div className="w-full h-full absolute bg-(--color-background)">
             <header className="flex justify-between items-center text-white py-6 px-8 md:px-32">
@@ -8,7 +12,14 @@ const Header = () => {
                     <li className="p-3 hover:underline underline-offset-4 rounded-md transition-all cursor-pointer">Home</li>
                     <li className="p-3 hover:underline underline-offset-4 rounded-md transition-all cursor-pointer">Produtos</li>
                 </ul>
-                <div className="relative hidden md:flex items-center justify-center gap-3"></div>
+
+                <i className="bx bx-menu xl:hidden block text-5xl cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+                <div className={`absolute xl:hidden top-24 left-0 w-full bg-(--color-background-100) flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${isMenuOpen ? "opacity-100" : "opacity-0"}`}
+                style={{transition: "transform 0.3s ease, opacity 0.3s ease"}}>
+                    <li className="list-none w-full text-center p-4 hover:underline underline-offset-4">Home</li>
+                    <li className="list-none w-full text-center p-4 hover:underline underline-offset-4">Produtos</li>
+
+                </div>
             </header>
         </div>
     );
